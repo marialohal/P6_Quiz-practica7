a48036d4719c2c8f57889ddbed368d237febfc3e
marialohal <maria-lopez94@hotmail.com>
2018-05-26 17:37:56 +0200

    version 7

---
+++ models/index.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+// Load ORM
+const Sequelize = require('sequelize');
+
+
+const url = process.env.DATABASE_URL || "sqlite:quiz.sqlite"; 
+
+const sequelize = new Sequelize(url);
+
+// Import the definition of the Quiz Table from quiz.js
+sequelize.import(path.join(__dirname, 'quiz'));
+
+// Import the definition of the Tips Table from tip.js
+sequelize.import(path.join(__dirname,'tip'));
+
+// Import the definition of the Users Table from user.js
+sequelize.import(path.join(__dirname,'user'));
+
+// Session
+sequelize.import(path.join(__dirname,'session'));
+
+const {quiz, tip, user} = sequelize.models;
+
+tip.belongsTo(quiz);
+quiz.hasMany(tip);
+
+user.hasMany(quiz, {foreignKey: 'authorId'});
+quiz.belongsTo(user, {as: 'author', foreignKey: 'authorId'});
+
+user.hasMany(tip, {foreignKey: 'authorId'});
+tip.belongsTo(user, {as: 'author', foreignKey: 'authorId'});
+
+
+module.exports = sequelize;
